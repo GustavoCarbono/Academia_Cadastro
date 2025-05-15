f4762b34fe5b02cd59a24ff1165a8f4c52be1121
GustavoCarbono <gustavo.s.c.820@gmail.com>
2025-05-14 22:13:19 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+let btn = document.querySelector('.button')
+
+btn.addEventListener('click', pegarDados)
+
+
+
+function pegarDados(){
+    let form = document.querySelector('form')
+
+    let pessoa = {
+        nome: form.nome.value,
+        email: form.email.value,
+        endereco: form.endereco.value,
+        plano: form.plano.value,
+        sexo: form.sexo.value,
+        cpf: form.cpf.value
+    }
+    
+    sessionStorage.pessoa = JSON.stringify(pessoa)
+    location.href = 'http://127.0.0.1:5500/confirmarCadastro.html'
+}
---
+++ js/script2.js
@@ -0,0 +1,19 @@
+
+const pessoa = JSON.parse(sessionStorage.pessoa)
+
+let spans = document.querySelectorAll('span')
+
+spans[0].textContent = pessoa.nome
+spans[1].textContent = pessoa.email
+spans[2].textContent = pessoa.endereco
+spans[3].textContent = pessoa.plano
+spans[4].textContent = pessoa.cpf
+spans[5].textContent = pessoa.sexo
+
+const img = document.querySelector('.impressora')
+
+img.addEventListener('click', ()=>{
+    img.style.display = 'none'
+    window.print()
+    img.style.display = 'block'
+})
